46900cd6b39efc2c4c8ad63063ab20e4442cc5b9
webdevKmer <webdevKmer@gmail.com>
2022-10-13 20:37:23 +0000

    finishing the save count functionnality and adding total functionnality

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 console.log("Hello from js.")
 let counter = document.getElementById('count')
 let step = 0
-counter.innerHTML = step
+let total = 0
+counter.textContent = step
 const addBtn = document.getElementById('add')
 const saveBtn = document.getElementById('save')
+const prevSaves = document.getElementById('prev-saves')
+const totalElt = document.getElementById('total-count')
+const totalBtn = document.getElementById('total')
 addBtn.addEventListener('click', () => {
     step += 1
-    counter.innerHTML = step
+    counter.textContent = step
 })
 function saveCount(){
-    console.log(counter.innerText)
+    console.log(counter.textContent)
+    total += parseInt(counter.textContent)
+    prevSaves.textContent += counter.textContent + ' - '
+    counter.textContent = 0
+    step = 0
+    return total
+}
+function getTotal(){
+    console.log(total)
+    totalElt.textContent = '( ' + total + ' )'
 }
 saveBtn.addEventListener('click', () => {
     saveCount()
+})
+totalBtn.addEventListener('click', () => {
+    getTotal()
 })
