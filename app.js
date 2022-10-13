7891d2427c310baa025bd35a2e86e4da253d56f6
webdevKmer <webdevKmer@gmail.com>
2022-10-13 18:48:50 +0000

    adding functionnality to the add person button

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello from js.")
+let counter = document.getElementById('count')
+let step = 0
+counter.innerHTML = step
+const btn = document.getElementById('add') 
+btn.addEventListener('click', () => {
+    step += 1
+    counter.innerHTML = step
+    console.log('click', counter.innerText)
+})
+console.log(counter.innerText)
